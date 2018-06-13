3c4e1cbeb8c979e547cea65cfaceca4c989938ef
Brevon Good <brevonbgood@comcast.net>
2018-06-13 13:28:44 -0600

    update knexfile for production

--- knexfile.js
+++ knexfile.js
@@ -3,4 +3,8 @@ module.exports = {
     client: 'pg',
     connection: 'postgres://localhost/dumpr'
   },
+  production: {
+    client: 'pg',
+    connection: `${process.env.DATABASE_URL}?ssl=true`
+  }
 };
